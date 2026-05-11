2c53b649a87fd435bbce260cac277bba803892a2
Sea <zzznn.chen@gmail.com>
2026-05-11 11:56:17 +0800

    美化上傳檔案

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -11,7 +11,7 @@ function FileUploader() {
     if (selectedFile) {
       const fileName = selectedFile.name.toLowerCase();
       const isZip = fileName.endsWith('.zip') || fileName.endsWith('.rar') || fileName.endsWith('.7z');
-      
+
       if (isZip) {
         setFile(selectedFile);
       } else {
@@ -48,31 +48,30 @@ function FileUploader() {
   }
 
   return (
-    <div>
-      <input 
-        type="file" 
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        accept=".zip,.rar,.7z"
-        style={{ display: 'none' }}
-      />
+    <div className='flex justify-center mt-28'>
+      <input type="file" ref={fileInputRef} onChange={handleFileChange} accept=".zip,.rar,.7z" className="hidden" />
 
-      {!file ? (
-        <button onClick={triggerSelect}>
-          <Upload /> 選擇檔案
-        </button>
-      ) : (
-        <div>
-          <FileArchive />
-          <span>{file.name}</span>
-          <button onClick={function() { setFile(null); }} disabled={uploading}>
-            <X />
+      {!file ?
+        (
+          <div className='flex flex-col items-center gap-8'>
+          <span className='text-3xl  tracking-widest'>點擊上傳壓縮檔</span>
+          <button className='border-6 rounded-3xl hover:text-blue-500 cursor-pointer' title='上傳壓縮檔' onClick={triggerSelect}>
+            <Upload className='size-36' />
           </button>
-          <button onClick={uploadFile} disabled={uploading}>
-            {uploading ? "執行中" : "開始上傳"}
-          </button>
-        </div>
-      )}
+          </div>
+        ) :
+        (
+          <div className='flex flex-col items-center gap-8'>
+            <span className='text-3xl  tracking-widest'>{file.name}</span>
+            <div className='flex flex-col items-center p-2 border-4 rounded-2xl'><FileArchive className='size-36' /></div>
+            <button onClick={function () { setFile(null); }} disabled={uploading}>
+              <X />
+            </button>
+            <button onClick={uploadFile} disabled={uploading}>
+              {uploading ? "執行中" : "開始上傳"}
+            </button>
+          </div>
+        )}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { File } from 'lucide-react';
 function Header() {
   return(
-    <header className="flex items-center h-20 px-16 gap-6 bg-white">
+    <header className="flex items-center w-full h-20 px-16 gap-6 bg-[#f9fafb]">
       <File className='text-[#4F46E5] size-14' strokeWidth={2} />
       <span className='text-3xl text-[#4F46E5] tracking-widest '>專案自動導讀及快速交接平台</span>
     </header>
