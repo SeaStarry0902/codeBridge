ce01a8d72e661c0bf7537dab49c47fe5ae849653
Sea <zzznn.chen@gmail.com>
2026-05-15 13:29:48 +0800

    按鈕美化

--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -7,22 +7,32 @@ function FileUploader() {
   const [selectedOptions, setSelectedOptions] = useState([]);
 
   const fileInputRef = useRef(null);
-  const optionsList = [
-    { id: 1, name: "1", apiUrl: "/upload" },
-    { id: 2, name: "2", apiUrl: "/upload-folder" },
-    { id: 3, name: "3", apiUrl: "/upload-github" },
-    { id: 4, name: "4", apiUrl: "/upload-github" },
-    { id: 5, name: "5", apiUrl: "/upload-github" },
-    { id: 6, name: "6", apiUrl: "/upload-github" },
-  ]
+  const [optionList, setOptionList] = useState([
+    { id: 1, name: "生成api使用文件", apiUrl: "/upload", selected: false },
+    { id: 2, name: "生成系統架構圖", apiUrl: "/upload-folder", selected: false },
+    { id: 3, name: "生成資料庫ER圖", apiUrl: "/upload-github", selected: false },
+    { id: 4, name: "生成專案建制指南", apiUrl: "/upload-github", selected: false },
+    { id: 5, name: "生成dockerfile", apiUrl: "/upload-github", selected: false },
+    { id: 6, name: "系統漏洞分析", apiUrl: "/upload-github", selected: false },
+  ]);
+  // const optionsList = [
+  //   { id: 1, name: "生成api使用文件", apiUrl: "/upload", selected: false },
+  //   { id: 2, name: "生成系統架構圖", apiUrl: "/upload-folder", selected: false },
+  //   { id: 3, name: "生成資料庫ER圖", apiUrl: "/upload-github", selected: false },
+  //   { id: 4, name: "生成專案建制指南", apiUrl: "/upload-github", selected: false },
+  //   { id: 5, name: "生成dockerfile", apiUrl: "/upload-github", selected: false },
+  //   { id: 6, name: "系統漏洞分析", apiUrl: "/upload-github", selected: false },
+  // ]
 
-  function handleCheckboxChange(id) {
-    if (selectedOptions.includes(id)) {
+  function handleCheckboxChange(opt) {
+    setOptionList(optionList.map(o => o.id === opt.id ? { ...o, selected: !o.selected } : o));
+    console.log(opt);
+    if (selectedOptions.includes(opt.id)) {
       setSelectedOptions(selectedOptions.filter(function (item) {
-        return item !== id;
+        return item !== opt.id;
       }));
     } else {
-      setSelectedOptions([...selectedOptions, id]);
+      setSelectedOptions([...selectedOptions, opt.id]);
     }
   }
 
@@ -57,6 +67,7 @@ function FileUploader() {
       setUploading(false);
       setSelectedOptions([]);
       fileInputRef.current.value = null;
+      setOptionList(optionList.map(opt => ({ ...opt, selected: false })));
     }
   }
 
@@ -81,26 +92,26 @@ function FileUploader() {
           <div className='flex flex-col items-center gap-8'>
             <span className='text-3xl  tracking-widest'>{file.name}</span>
             <div className='flex flex-col items-center p-2 border-4 rounded-2xl'><FileArchive className='size-36' /></div>
-            <button onClick={function () { setFile(null); fileInputRef.current.value = null; }} disabled={uploading}>
-              <X />
-            </button>
-
-            <div>
-              {optionsList.map(function (opt) {
+            <div className='grid grid-cols-3 gap-2'>
+              {optionList.map(function (opt) {
                 return (
-                  <label key={opt.id} className='flex'>
-                    <input type="checkbox" checked={selectedOptions.includes(opt.id)} onChange={function () { handleCheckboxChange(opt.id) }} />
-                    {opt.name}
+                  <label key={opt.id} className={`flex items-center w-auto h-10 px-2 gap-2 border-2 rounded-sm cursor-pointer transition-colors hover:bg-gray-200 ${opt.selected == true ? 'bg-gray-200 ' : 'bg-white '}`}>
+                    <input type="checkbox" className='size-4' checked={selectedOptions.includes(opt.id)} onChange={function () { handleCheckboxChange(opt) }} />
+                    <span className=' tracking-wider'>{opt.name}</span>
                   </label>
 
                 )
               })
               }
             </div>
-
-            <button onClick={uploadFile} disabled={uploading}>
-              {uploading ? "執行中" : "開始上傳"}
-            </button>
+            <div className="flex justify-center w-full gap-4">
+              <button className='flex-1 p-2 w-80 bg-blue-500 text-white hover:bg-blue-700 hover:text-black cursor-pointer border border-black rounded-sm' onClick={uploadFile} disabled={uploading}>
+                {uploading ? "執行中" : "開始上傳"}
+              </button>
+              <button className='group p-2 border rounded-sm bg-red-500 hover:bg-red-700 cursor-pointer ' onClick={function () { setFile(null); fileInputRef.current.value = null; }} disabled={uploading}>
+                <X className='size-6 text-white group-hover:text-black' />
+              </button>
+            </div>
           </div>
         )}
     </div>
